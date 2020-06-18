8c90f6c51e8e8e83dbe0f2e4030bead4f4317189
TheGingeraffe <hestonio@gmail.com>
2020-06-18 14:25:05 -0700

    Added TODOs

---
+++ skc.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# skc.sh - (S)SH (K)ey (C)reator
+
+# Check permissions on .ssh dir and contents
+
+# Check known_hosts?
+
+# Check authorized_keys?
+
+# Point at a config file?
+
+# Check if keys in ssh config exist?
+
+# Check if remote servers support Ed25519 keys?
+# Check if remote servers support password auth?
+
+# Store passphrase/key in Vault
