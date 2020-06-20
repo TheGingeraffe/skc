2999e71a7cb2b2509907b0422299be5800a47d4c
TheGingeraffe <hestonio@gmail.com>
2020-06-19 17:55:36 -0700

    Added permcheck for .ssh

--- skc.sh
+++ skc.sh
@@ -3,8 +3,8 @@
 
 ## Functions/Variables/Arrays
 
-## General purpose confirmation
-## Allows a set number of attempts
+# General purpose confirmation
+# Allows a set number of attempts
 confirm() {
 
 	conf_msg="${1}"
@@ -33,7 +33,9 @@ confirm() {
 
 }
 
-# empty_var_check "USER"
+# Checks an arbitrary list of strings to see if they are set/unset
+# returns 1 if set, 0 if unset
+# usage: empty_var_check "USER"
 empty_var_check() {
 	for var in "${@}"; do
     	if [[ -v "${var}" ]]; then
@@ -45,29 +47,40 @@ empty_var_check() {
 	done
 }
 
-# Check for running ssh-agent and start if desired
+# Tests for existence
 
-empty_var_check SSH_AUTH_SOCK
 
-if [[ $? -eq 0 ]]; then
-  confirm "ssh-agent not running / \$SSH_AUTH_SOCK is empty! Would you like to start ssh-agent?"
-  if [[ $? -eq 0 ]]; then
-    agent_pid="$(echo $(eval $(ssh-agent)) | cut -f3 -d' ')"
-  fi
-fi
-echo $agent_pid
+# Checks a file or dir permissions
 
-# Check for added keys
-# start if not running
-# add created keys to agent
+perm_check() {
+	for arg in "${@}"; do
+	  permission_var="$(stat -c "%a" "${arg}")"
+	  echo ${permission_var}
+	done
+
+}
+
+perm_check "$@"
 
 # Check permissions on .ssh dir and contents
 # .ssh - 700 or 755?
+
+if [[ -d "/home/${USER}/.ssh" ]]; then
+	perm_check "/home/${USER}/.ssh"
+	if [[ ! "${permission_var}" = "755" ]]; then
+	echo "Permissions for /home/${USER}/.ssh are not 755!"
+	fi
+else
+	echo "/home/${USER}/.ssh does not exist or is not readable!"
+fi
+
 # .pub keys - 644
 # private keys - 600
 # home dir - 750 (not writeable by group/others)
 # ~/.ssh/known_hosts - 600
 # ~/.ssh/authorized_keys - 600
+
+
 
 # Check ~/.ssh/known_hosts?
 
@@ -83,3 +96,23 @@ echo $agent_pid
 # Audit connected servers? Edit ssh config file to best practices?
 
 # Store passphrase/key in Vault
+
+
+# Check for running ssh-agent
+
+# empty_var_check SSH_AUTH_SOCK
+
+# Asks to start ssh-agent if $SSH_AUTH_SOCK unset
+
+if [[ $? -eq 0 ]]; then
+  confirm "ssh-agent not running / \$SSH_AUTH_SOCK is empty! Would you like to start ssh-agent?"
+fi  
+
+# Starts ssh-agent and stores PID
+
+if [[ $? -eq 0 ]]; then
+  agent_pid="$(echo $(eval $(ssh-agent)) | cut -f3 -d' ')"
+fi
+
+# Check for added keys
+# add created keys to agent
